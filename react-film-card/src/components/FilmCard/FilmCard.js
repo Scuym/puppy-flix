c1a7150ed6b0e9cbf5b3657d548291cfd475f9c6
tmossion <tanguymossion@orange.fr>
2023-01-31 16:29:40 +0100

    Carte presque terminee manque paralax

--- react-film-card/src/components/FilmCard/FilmCard.js
+++ react-film-card/src/components/FilmCard/FilmCard.js
@@ -4,15 +4,13 @@ import "./FilmCard.css";
 class FilmCard extends React.Component {
     render() {
         return (
-            <div className="card">
+            <div className="card" style={{ backgroundImage: "url(/background.jpg)" }}>
                 <div className="bottom-banner">
                     <div className="left">
-                        <i>
-
-                        </i>
+                        <svg className="play-button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M188.3 147.1C195.8 142.8 205.1 142.1 212.5 147.5L356.5 235.5C363.6 239.9 368 247.6 368 256C368 264.4 363.6 272.1 356.5 276.5L212.5 364.5C205.1 369 195.8 369.2 188.3 364.9C180.7 360.7 176 352.7 176 344V167.1C176 159.3 180.7 151.3 188.3 147.1V147.1zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"/></svg>
                         <div className="film-info">
                             <h3 className="film-title">
-                                Titre de film
+                                The Last Of Us
                             </h3>
                             <p className="film-genre">
                                 Drama / Western
@@ -20,9 +18,8 @@ class FilmCard extends React.Component {
                         </div>
                     </div>
                     <div className="right">
-                        <i>
-
-                        </i>
+                        <div className="divider"></div>
+                        <svg className="rate" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
                         <p className="film-rate">
                             4.8
                         </p>
